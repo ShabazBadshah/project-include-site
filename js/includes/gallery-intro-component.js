7f1839afdc31d2fe8d2eb6f63b552d8bbecdc042
ShabazBadshah <badshah.shabaz@gmail.com>
2019-12-17 21:48:47 -0500

    Closed #2. Implemented gallery-intro-component

---
+++ js/includes/gallery-intro-component.js
@@ -0,0 +1,27 @@
+const ROOT_IMG_DIR = "assets/gallery/homepage/";
+const TRANSITION_DELAY_MS = 2000;
+
+var galleryItem;
+
+var appGalleryImageSrc = [
+  ROOT_IMG_DIR + "2.png",
+  ROOT_IMG_DIR + "4.png",
+  ROOT_IMG_DIR + "1.png",
+  ROOT_IMG_DIR + "3.png"
+];
+
+$(document).ready(function() {
+  galleryItem = $("img.intro-container--gallery-container--gallery");
+  var currImageIdx = 0;
+  var lengthAppGalleryImages = appGalleryImageSrc.length;
+
+  setInterval(function() {
+    galleryItem
+      .fadeTo(TRANSITION_DELAY_MS, 0.1, function() {
+        galleryItem.attr("src", appGalleryImageSrc[currImageIdx]);
+        currImageIdx %= lengthAppGalleryImages;
+        currImageIdx++;
+      })
+      .fadeTo(TRANSITION_DELAY_MS, 1);
+  }, TRANSITION_DELAY_MS * 3);
+});
