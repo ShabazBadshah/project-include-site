9131cb544296925ea3fabb6875ca7c5f1b77b2c2
ShabazBadshah <badshah.shabaz@gmail.com>
2019-12-25 18:29:09 -0500

    Moved some header styles to common stylesheet

--- js/includes/header-component.js
+++ js/includes/header-component.js
@@ -1,20 +1,16 @@
 const HEADER_COMPONENT_TRANSITION_MS = 250;
 const hamburgerMenuIcon = $("#hamburger-menu-icon");
+const mobileNav = $(".mobile-nav-bar > #social-nav-list-container");
 
 $(document).ready(function() {
   let isOpen = true;
 
   hamburgerMenuIcon.click(function() {
     isOpen = !isOpen;
-    if (!isOpen) {
-      $(".mobile-nav-bar > #social-nav-list-container").show(
-        HEADER_COMPONENT_TRANSITION_MS
-      );
-    } else {
-      $(".mobile-nav-bar > #social-nav-list-container").hide(
-        HEADER_COMPONENT_TRANSITION_MS
-      );
-    }
-    $(this).toggleClass("is-active");
+    !isOpen
+      ? mobileNav.show(HEADER_COMPONENT_TRANSITION_MS)
+      : mobileNav.hide(HEADER_COMPONENT_TRANSITION_MS);
+
+    hamburgerMenuIcon.toggleClass("is-active");
   });
 });
